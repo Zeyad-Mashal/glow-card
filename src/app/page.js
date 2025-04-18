2a81c4eeb2118896f246784a47ff23863aabd775
Mashal <zyadomar112@gmail.com>
2025-04-18 14:51:50 +0200

    update eslint

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Hello, I'm Glow Card</h1>
-      <p className="mt-4 text-lg">I am a digital business card.</p>
+      <p className="mt-4 text-lg">I&apos;m a digital business card.</p>
       <Image
         src={img}
         alt="Glow Card"
