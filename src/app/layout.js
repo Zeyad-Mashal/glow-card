8fa24f8c4092d705e6118e1ce9d0e2889af0e1de
Mashal <zyadomar112@gmail.com>
2025-04-18 18:02:42 +0200

    1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Glow Card",
   description: "Hello, I'm Glow Card. I am a digital business card.",
@@ -19,11 +7,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
-    </html>
+    </html >
   );
 }
