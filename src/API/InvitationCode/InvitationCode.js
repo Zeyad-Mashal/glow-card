9fc36a49fd0a2869652f7f211c062af84707bde5
Mashal <zyadomar112@gmail.com>
2025-05-08 01:46:57 +0300

    update code

---
+++ src/API/InvitationCode/InvitationCode.js
@@ -0,0 +1,42 @@
+const URL = "https://glow-card.onrender.com/api/v1/user/info";
+
+const InvitationCode = async (setloading, setError, setCode) => {
+    setloading(true);
+
+    let token;
+    if (typeof window !== 'undefined') {
+        token = localStorage.getItem('token');
+    } else {
+        token = null; // لو عايز، ممكن تتعامل مع null بغير طريقة
+    }
+
+    try {
+        const response = await fetch(URL, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `glowONW${token}`
+            },
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            setloading(false);
+            setCode(result.invitationCode);
+        } else {
+            if (response.status == 404) {
+                setError(result.message);
+            } else if (response.status == 500) {
+                console.log(result.message);
+                setError(result.message);
+            }
+            setloading(false);
+        }
+    } catch (error) {
+        setError('An error occurred');
+        setloading(false);
+    }
+};
+
+export default InvitationCode;
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,21 +1,33 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./profile.css";
-
+import InvitationCode from "@/API/InvitationCode/InvitationCode";
 const Profile = () => {
-  const [activeTab, setActiveTab] = useState("card_info"); // الحالة لتحديد الجزء المفتوح
-
+  useEffect(() => {
+    getInviteCode();
+  }, []);
+  const [activeTab, setActiveTab] = useState("card_info");
   const [toastMessage, setToastMessage] = useState("");
+  const [controller, setController] = useState(true);
+  const [showContentOnMobile, setShowContentOnMobile] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [code, setCode] = useState("");
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleCopy = () => {
-    const link = "https://example.com/invite/123456";
-
-    // نسخ الرابط إلى الحافظة
+    const link = `https://glow-card.vercel.app/login?code=${code}`;
     navigator.clipboard.writeText(link).then(() => {
-      // إظهار رسالة التوستر
       setToastMessage("تم نسخ الرابط بنجاح!");
-
-      // إخفاء التوستر بعد 3 ثواني
       setTimeout(() => {
         setToastMessage("");
       }, 3000);
@@ -23,100 +35,118 @@ const Profile = () => {
   };
 
   const handleTabClick = (tab) => {
-    setActiveTab(tab); // تغيير الجزء المفتوح عند الضغط
+    setActiveTab(tab);
+    if (windowWidth < 768) {
+      setShowContentOnMobile(true);
+    }
+  };
+
+  const getInviteCode = () => {
+    InvitationCode(setLoading, setError, setCode);
   };
 
   return (
     <div className="profile">
       <div className="profile_container">
-        <div className="profile_controller">
-          <div className="profile_img">
-            <img src="/images/user.png" alt="" />
+        {controller && (!showContentOnMobile || windowWidth >= 768) && (
+          <div className="profile_controller">
+            <div className="profile_img">
+              <img src="/images/user.png" alt="" />
+            </div>
+            <h2>Zeyad Mashaal</h2>
+            <div className="controller_tabs">
+              <div
+                className={`controller_tab ${
+                  activeTab === "card_info" ? "active" : ""
+                }`}
+                onClick={() => handleTabClick("card_info")}
+              >
+                Card Info
+              </div>
+              <div
+                className={`controller_tab ${
+                  activeTab === "invite_link" ? "active" : ""
+                }`}
+                onClick={() => handleTabClick("invite_link")}
+              >
+                Invitation link
+              </div>
+              <div
+                className={`controller_tab ${
+                  activeTab === "logout" ? "active" : ""
+                }`}
+                onClick={() => handleTabClick("logout")}
+              >
+                Log Out
+              </div>
+            </div>
           </div>
-          <h2>Zeyad Mashaal</h2>
-          <div className="controller_tabs">
-            <div
-              className={`controller_tab ${
-                activeTab === "card_info" ? "active" : ""
-              }`}
-              onClick={() => handleTabClick("card_info")}
-            >
-              Card Info
-            </div>
-            <div
-              className={`controller_tab ${
-                activeTab === "invite_link" ? "active" : ""
-              }`}
-              onClick={() => handleTabClick("invite_link")}
-            >
-              Invitation link
-            </div>
-            <div
-              className={`controller_tab ${
-                activeTab === "logout" ? "active" : ""
-              }`}
-              onClick={() => handleTabClick("logout")}
-            >
-              Log Out
-            </div>
-          </div>
-        </div>
-        <div className="profile_content">
-          {/* Card Info */}
-          {activeTab === "card_info" && (
-            <div className="profile_content_card card_info">
-              <h2>Card Info</h2>
-              <div className="card_info">
-                <img src="/images/cardfront.png" alt="" />
-                <div className="card_info_item">
-                  <h3>Personal Card</h3>
-                  <p>Zeyad Mashaal</p>
-                  <div className="card_info_item_date">
-                    <span>12 / 4 / 2024</span>
-                    <p>subscription date was </p>
-                  </div>
-                  <div className="card_info_item_date">
-                    <span>12 / 4 / 2024</span>
-                    <p>Expire date at </p>
+        )}
+
+        {(!controller || showContentOnMobile || windowWidth >= 768) && (
+          <div className="profile_content">
+            {/* Back button for mobile */}
+            {windowWidth < 768 && showContentOnMobile && (
+              <button
+                className="back_button"
+                onClick={() => setShowContentOnMobile(false)}
+              >
+                <span>&lt;--</span> Back to Profile
+              </button>
+            )}
+
+            {activeTab === "card_info" && (
+              <div className="profile_content_card card_info">
+                <h2>Card Info</h2>
+                <div className="card_info">
+                  <img src="/images/cardfront.png" alt="" />
+                  <div className="card_info_item">
+                    <h3>Personal Card</h3>
+                    <p>Zeyad Mashaal</p>
+                    <div className="card_info_item_date">
+                      <span>12 / 4 / 2024</span>
+                      <p>subscription date was</p>
+                    </div>
+                    <div className="card_info_item_date">
+                      <span>12 / 4 / 2024</span>
+                      <p>Expire date at</p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
 
-          {/* Invitation Link */}
-          {activeTab === "invite_link" && (
-            <div className="profile_content_card invite_link">
-              <h2>Invitation Link</h2>
-              <p>
-                شارك هذا الرابط مع صديقك للحصول على شهرين مجانيين في حال اشتراكه
-                معنا
-              </p>
-              <div className="invitation_link">
-                <p>https://example.com/invite/123456</p>
-                <button onClick={handleCopy}>Copy</button>
-              </div>
-
-              {toastMessage && (
-                <div className="toast">
-                  <p>{toastMessage}</p>
+            {activeTab === "invite_link" && (
+              <div className="profile_content_card invite_link">
+                <h2>Invitation Link</h2>
+                <p>
+                  شارك هذا الرابط مع صديقك للحصول على شهرين مجانيين في حال
+                  اشتراكه معنا
+                </p>
+                <div className="invitation_link">
+                  <p>https://glow-card.vercel.app/login?code={code}</p>
+                  <button onClick={handleCopy}>Copy</button>
                 </div>
-              )}
-            </div>
-          )}
+                {toastMessage && (
+                  <div className="toast">
+                    <p>{toastMessage}</p>
+                  </div>
+                )}
+              </div>
+            )}
 
-          {/* Log Out */}
-          {activeTab === "logout" && (
-            <div className="profile_content_card logout">
-              <h2>Log Out</h2>
-              <p>
-                <span>انتبه!!</span> في حال خروجك من هذا الحساب لن تستطيع مشاهدة
-                البطاقة إلا عند التسجيل مرة اخرى!!!
-              </p>
-              <button className="logout_button">Log Out</button>
-            </div>
-          )}
-        </div>
+            {activeTab === "logout" && (
+              <div className="profile_content_card logout">
+                <h2>Log Out</h2>
+                <p>
+                  <span>انتبه!!</span> في حال خروجك من هذا الحساب لن تستطيع
+                  مشاهدة البطاقة إلا عند التسجيل مرة اخرى!!!
+                </p>
+                <button className="logout_button">Log Out</button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
