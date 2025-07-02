105b99f74de17f17d690dc3dab2ee5f44eb6a5f5
Mashal <zyadomar112@gmail.com>
2025-07-02 21:25:33 +0300

    update lastest

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,31 +1,35 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import "./New.css";
+import Link from "next/link";
+import LatestFoundation from "@/API/LatestFoundation/LatestFoundation";
 const page = () => {
+  useEffect(() => {
+    getLatestFoundations();
+  }, []);
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState("");
+  const [allLatestFoundation, setAllLatestFoundation] = useState([]);
+  const getLatestFoundations = () => {
+    LatestFoundation(setloading, setError, setAllLatestFoundation);
+  };
   return (
     <div className="new">
       <div className="new_container">
         <h1>انضم حديثا</h1>
-        <p>
-          This is the new page. It is currently under construction. Please check
-          back later for updates.
-        </p>
         <div className="new_list">
-          <div className="new_item">
-            <img src="/images/network1.jpeg" alt="new comes" />
-            <h2>New Network</h2>
-          </div>
-          <div className="new_item">
-            <img src="/images/network1.jpeg" alt="new comes" />
-            <h2>New Network</h2>
-          </div>
-          <div className="new_item">
-            <img src="/images/network1.jpeg" alt="new comes" />
-            <h2>New Network</h2>
-          </div>
-          <div className="new_item">
-            <img src="/images/network1.jpeg" alt="new comes" />
-            <h2>New Network</h2>
-          </div>
+          {loading
+            ? "Loading..."
+            : allLatestFoundation.map((item, index) => {
+                return (
+                  <Link href={`/foundation-details?id=${item._id}`}>
+                    <div className="new_item" key={index}>
+                      <img src={item.images[0]} alt="new comes" />
+                      <h2>{item.name}</h2>
+                    </div>
+                  </Link>
+                );
+              })}
         </div>
       </div>
     </div>
